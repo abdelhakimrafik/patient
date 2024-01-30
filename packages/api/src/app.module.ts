4192d220e411d8cc4f2496edcecb1d777c5721cc
Abdelhakim RAFIK <ra.abdelhakim@gmail.com>
2024-01-30 03:27:31 +0100

    feat(api): added supporting packages

--- packages/api/src/app.module.ts
+++ packages/api/src/app.module.ts
@@ -1,10 +1,36 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { DatabaseModule } from './database/database.module';
+import { UsersModule } from './users/users.module';
+import { PatientsModule } from './patients/patients.module';
+import { ConfigModule } from '@nestjs/config';
+import { AuthModule } from './auth/auth.module';
+import appConfig from './config/app.config';
+import databaseConfig from './config/database.config';
+import jwtConfig from './config/jwt.config';
+import { APP_GUARD } from '@nestjs/core';
+import { AuthGuard } from './auth/guards/auth.guard';
+import { JwtModule } from '@nestjs/jwt';
+import { RedisModule } from './redis/redis.module';
+import redisConfig from './config/redis.config';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      load: [appConfig, databaseConfig, jwtConfig, redisConfig],
+    }),
+    JwtModule.registerAsync(jwtConfig.asProvider()),
+    DatabaseModule,
+    UsersModule,
+    PatientsModule,
+    AuthModule,
+    RedisModule,
+  ],
+  providers: [
+    {
+      provide: APP_GUARD,
+      useClass: AuthGuard,
+    },
+  ],
 })
 export class AppModule {}
--- packages/api/src/main.ts
+++ packages/api/src/main.ts
@@ -1,8 +1,41 @@
 import { NestFactory } from '@nestjs/core';
+import { ValidationPipe } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  /** Configure validator **/
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      transform: true,
+      forbidNonWhitelisted: true,
+      transformOptions: {
+        enableImplicitConversion: true,
+      },
+    }),
+  );
+
+  /** Setup swagger **/
+  const config = new DocumentBuilder()
+    .setTitle('Patient API')
+    .setDescription('The patient API')
+    .setVersion('1.0.1')
+    .addBearerAuth(
+      { type: 'http', scheme: 'bearer', bearerFormat: 'JWT' },
+      'access-token',
+    )
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('docs', app, document);
+
+  const configService = app.get(ConfigService);
+  const port = configService.get<number>('app.port');
+
+  await app.listen(port);
 }
+
 bootstrap();
