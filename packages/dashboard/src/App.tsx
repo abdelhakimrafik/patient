d9c34dea208b746d82ebac6d8204b1f9e44d8e4e
Abdelhakim RAFIK <ra.abdelhakim@gmail.com>
2024-01-31 04:16:58 +0100

    feat(dasboard): implement routing

--- packages/dashboard/src/App.tsx
+++ packages/dashboard/src/App.tsx
@@ -1,10 +1,36 @@
-import Layout from './common/Layout';
-import Login from './pages/Login';
+import { lazy } from 'react';
+import Layout from './Layout';
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+import ErrorPage from './pages/ErrorPage';
+
+/**
+ * Public pages
+ */
+const Login = lazy(() => import('./pages/Login'));
+
+/**
+ * Private pages
+ */
+const PatientsList = lazy(() => import('./pages/Patients/PatientsList'));
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Layout />,
+    errorElement: <ErrorPage />,
+    children: [
+      {
+        path: '/login',
+        element: <Login />,
+      },
+      {
+        path: '/',
+        element: <PatientsList />,
+      },
+    ],
+  },
+]);
 
 export default function App() {
-  return (
-    <Layout style={{ padding: 15 }}>
-      <Login />
-    </Layout>
-  );
+  return <RouterProvider router={router} />;
 }
